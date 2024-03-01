187b0a9605f8200863b6a117212e0a83124b81c6
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-02-29 23:18:50 -0300

    Remove unused app-env.d.ts and add StatusBar to _layout.tsx and Logo to index.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 import { Box } from '@/components/Box'
 import { Slot } from 'expo-router'
+import { StatusBar } from 'react-native'
 import '../global.css'
 
 export default function Layout() {
   return (
     <Box>
+      <StatusBar barStyle="light-content" />
       <Slot />
     </Box>
   )
--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,11 @@
-import { Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
+
+import Logo from '@/assets/logo.png'
 
 export default function Page() {
   return (
     <View>
+      <Image source={Logo} alt="Logo" className="self-center" />
       <Text>Home</Text>
     </View>
   )
