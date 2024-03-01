4980f05c807b4c673e08d0b45a900df02b66ec3b
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-03-01 02:42:48 -0300

    Refactor Cards component to include completed status and title

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,9 +1,25 @@
 import { Text, View } from 'react-native'
 
-export function Cards() {
+import { Feather } from '@expo/vector-icons'
+
+type CardsProps = {
+  isCompleted: boolean
+  title: string
+}
+
+export function Cards({ isCompleted, title }: CardsProps) {
+  const cardCompleted = isCompleted ? 'bg-secondary' : 'bg-primary'
+
+  const cardText = isCompleted && 'line-through text-gray-500'
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-2xl font-bold">Cards</Text>
+    <View
+      className={`bg-primary ${cardCompleted} flex-row items-center justify-between py-7 px-3 rounded-lg`}>
+      <Text className={`text-xl ${cardText} font-bold text-white`}>
+        {title}
+      </Text>
+
+      {isCompleted && <Feather name="check" size={20} color="white" />}
     </View>
   )
 }
