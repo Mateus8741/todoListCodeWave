41ce914ffefd2f20c2b61e7e57a519dc1ba3c046
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-03-01 03:46:23 -0300

    Add TouchableOpacityProps to CustonButton

--- src/components/CustonButton.tsx
+++ src/components/CustonButton.tsx
@@ -1,10 +1,13 @@
-import { Text, TouchableOpacity } from 'react-native'
+import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native'
 
-export function CustonButton() {
+type CustonButtonProps = TouchableOpacityProps
+
+export function CustonButton({ ...rest }: CustonButtonProps) {
   return (
     <TouchableOpacity
       className="h-12 w-12 bg-lemon rounded-md items-center justify-center"
-      activeOpacity={0.7}>
+      activeOpacity={0.7}
+      {...rest}>
       <Text className="text-black font-bold text-2xl">+</Text>
     </TouchableOpacity>
   )
