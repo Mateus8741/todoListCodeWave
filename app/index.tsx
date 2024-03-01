9cf8c1c2e52a734440acc16bc0207b5e300c904c
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-03-01 03:58:37 -0300

    Add todo functionality to Page component

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,17 @@ import { FlatList, Image, ListRenderItemInfo, View } from 'react-native'
 
 import Logo from '@/assets/logo.png'
 import { Cards, CardsProps, CustonButton, Input } from '@/components'
+import { useState } from 'react'
 
 export default function Page() {
+  const [todos, setTodos] = useState<CardsProps[]>([])
+
+  function saveTodo() {
+    if (todos[0]?.title) {
+      setTodos((oldState) => [...oldState, todos[0]])
+    }
+  }
+
   function renderItem({ item }: ListRenderItemInfo<CardsProps>) {
     return <Cards {...item} />
   }
@@ -13,18 +22,18 @@ export default function Page() {
       <Image source={Logo} alt="Logo" className="self-center" />
 
       <View className="items-center mt-7 gap-x-3 flex-row">
-        <Input placeholder="Adicione seu todo..." />
-        <CustonButton />
+        <Input
+          placeholder="Adicione seu todo..."
+          onChangeText={(value) =>
+            setTodos([{ title: value, isCompleted: false }])
+          }
+          value={todos[0]?.title || ''}
+        />
+        <CustonButton onPress={saveTodo} />
       </View>
 
       <FlatList
-        data={[
-          { title: 'Fazer café', isCompleted: true },
-          { title: 'Estudar React Native', isCompleted: false },
-          { title: 'Estudar Next.js', isCompleted: false },
-          { title: 'Estudar TypeScript', isCompleted: true },
-          { title: 'Estudar Node.js', isCompleted: false },
-        ]}
+        data={cards}
         keyExtractor={(item) => item.title}
         renderItem={renderItem}
         className="mt-7 px-3"
@@ -32,3 +41,11 @@ export default function Page() {
     </>
   )
 }
+
+const cards = [
+  { title: 'Fazer café', isCompleted: true },
+  { title: 'Estudar React Native', isCompleted: false },
+  { title: 'Estudar Next.js', isCompleted: false },
+  { title: 'Estudar TypeScript', isCompleted: true },
+  { title: 'Estudar Node.js', isCompleted: false },
+]
