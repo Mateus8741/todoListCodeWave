cd4d094a3ea3e36f190767a7efd0b600a79b5231
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-03-01 02:07:52 -0300

    Add custom button component and update layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,18 @@
 import { Image, View } from 'react-native'
 
 import Logo from '@/assets/logo.png'
+import { CustonButton } from '@/components/CustonButton'
 import { Input } from '@/components/TextInput'
 
 export default function Page() {
   return (
-    <View>
+    <>
       <Image source={Logo} alt="Logo" className="self-center" />
-      <Input placeholder="Adicione seu todo..." />
-    </View>
+
+      <View className="items-center mt-7 gap-x-3 flex-row">
+        <Input placeholder="Adicione seu todo..." />
+        <CustonButton />
+      </View>
+    </>
   )
 }
